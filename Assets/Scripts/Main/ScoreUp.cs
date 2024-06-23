4e0309cead54ee242c34c991ca6aca1eb06ff175
AoiTomiyama <TadoO.o2209@gmail.com>
2024-06-23 12:26:48 +0900

    解像度をフルHDから960x540に変更

--- Assets/Scripts/Main/ScoreUp.cs
+++ Assets/Scripts/Main/ScoreUp.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 /// </summary>
 public class ScoreUp : MonoBehaviour
 {
-    float theta;
+    [Header("サイズ変化の倍率（EnableScaling有効時のみ）")]
+    [SerializeField]
+    float _scaleMultiply = 0.004f;
+    [Header("移動するスピード")]
+    [SerializeField]
+    float _moveSpeed = 0.01f;
+    float _theta;
     [SerializeField]
     bool _enableScaling = false;
     private void Start()
@@ -14,11 +20,11 @@ public class ScoreUp : MonoBehaviour
     }
     void Update()
     {
-        theta += Time.deltaTime;
-        transform.position = new Vector2(transform.position.x, transform.position.y + 0.01f * Mathf.Cos(3 * theta));
+        _theta += Time.deltaTime;
+        transform.position = new Vector2(transform.position.x, transform.position.y + _moveSpeed * Mathf.Cos(3 * _theta));
         if (_enableScaling)
         {
-            transform.localScale += Vector3.one * 0.004f;
+            transform.localScale += Vector3.one * _scaleMultiply;
         }
     }
 }
