b074a787af78a73567a7f50ba383990a893ec613
AoiTomiyama <TadoO.o2209@gmail.com>
2024-06-23 21:02:10 +0900

    同じ色の球がつながったときと、スタートカウント時のSEを追加。

--- Assets/Scripts/Main/StartCount.cs
+++ Assets/Scripts/Main/StartCount.cs
@@ -11,6 +11,9 @@ public class StartCount : MonoBehaviour
 {
     private float _startTime = 3;
     Text _startCountText;
+    AudioSource _aus;
+    [SerializeField]
+    AudioClip _countEndSE;
 
     public float StartTimer { get => _startTime; }
 
@@ -18,6 +21,7 @@ public class StartCount : MonoBehaviour
     {
         _startCountText = GetComponent<Text>();
         _startCountText.text = "";
+        _aus = GetComponent<AudioSource>();
         StartCoroutine(CountThreeSeconds());
     }
 
@@ -25,7 +29,9 @@ public class StartCount : MonoBehaviour
     {
         Debug.Log("Timer Started");
         yield return new WaitForSeconds(0.8f);
+        _aus.Play();
         this.gameObject.SetActive(true);
+        int timeMemo = Mathf.CeilToInt(StartTimer);
         while (StartTimer > 0)
         {
             yield return null;
@@ -33,12 +39,19 @@ public class StartCount : MonoBehaviour
             _startCountText.text = Mathf.Ceil(StartTimer).ToString();
             _startCountText.gameObject.SetActive(true);
 
+            if (timeMemo != Mathf.CeilToInt(StartTimer))
+            {
+                _aus.Play();
+            }
+            timeMemo = Mathf.CeilToInt(StartTimer);
+
             //カウントダウンの表示が、タイマーが自然数の時に大きさが最大、それ以外の時は徐々に縮小するように
             _startCountText.gameObject.transform.localScale = Vector3.one * (1.2f + Mathf.Ceil(StartTimer) - StartTimer);
 
             _startTime -= Time.deltaTime;
         }
         Debug.Log("Timer complete!");
+        _aus.PlayOneShot(_countEndSE);
         _startCountText.gameObject.transform.localScale = Vector3.one * 1.3f;
         _startCountText.text = "START!";
         Destroy(this.gameObject, 1);
